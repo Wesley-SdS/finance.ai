87ed64ba273e75762af2a6b08c884b8a3d2a3d0b
Wesley-SdS <wesleysantos.0095@gmail.com>
2024-11-09 09:49:40 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 const Home = () => {
-  return(
-    <div className="">Ola</div>
-  )
-}
+  return (
+  <div className="flex w-screen items-center justify-center">
+    Ola
+  </div>
+  ) ;
+};
 
 export default Home;
